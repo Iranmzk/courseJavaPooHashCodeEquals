6fc365e4cf211962af25351363aa83f54a2ef664
iranmzk <iranmpc93@gmail.com>
2022-05-09 14:02:45 -0300

    ajustes finos

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,20 @@ public class Program {
         //Tratamento especial para literais = compila verdadeiro
         //se o String fosse estanciado daria falso pois alocaria
         // referencias diferentes na memória.
-        String s1 = "teste";
-        String s2 = "teste";
+        String s1 = "Teste";
+        String s2 = "Teste";
 
         System.out.println(c1.hashCode());
         System.out.println(c2.hashCode());
+
+        System.out.println("--------------------------------");
+
         //Comparando refêrencia do conteúdo = compila verdadeiro
         System.out.println(c1.equals(c2));
+        System.out.println(s1.equals(s2));
+
+        System.out.println("--------------------------------");
+
         //Comparando refêrencia de memória = compila falso
         System.out.println(c1 == c2);
         System.out.println(s1 == s2);
